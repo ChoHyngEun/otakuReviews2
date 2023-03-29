ddb234690687b993d36d4439681e18e16afefba8
javaLearner13 <javalearner13@gmail.com>
2023-03-30 01:24:23 +0900

    added facets

--- src/main/java/com/example/ex3/interceptor/WebMvcConfig.java
+++ src/main/java/com/example/ex3/interceptor/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/signup", "/otp-auth", "/logout", "/guestbook/list", "/guestbook/*");
+                .excludePathPatterns("/login", "/signup", "/otp-auth", "/guestbook/list", "/guestbook/*");
     }
 
     @Override
