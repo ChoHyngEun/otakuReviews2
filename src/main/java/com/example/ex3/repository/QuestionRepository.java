faba88e91d1a68ed27b059aef3678ca11bf7c46c
javaLearner13 <javalearner13@gmail.com>
2023-04-09 11:55:03 +0900

    css reset

--- src/main/java/com/example/ex3/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ex3.repository;
-
-import com.example.ex3.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
